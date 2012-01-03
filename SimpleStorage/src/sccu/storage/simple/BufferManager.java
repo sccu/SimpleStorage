635111e5da77c37c585a3d3449a2d103263ce5df
Jhon Jang <sccu76@gmail.com>
2012-01-03 10:39:07 +0900

    writes close() in a BufferManager class

--- SimpleStorage/src/sccu/storage/simple/BufferManager.java
+++ SimpleStorage/src/sccu/storage/simple/BufferManager.java
@@ -2,21 +2,28 @@ package sccu.storage.simple;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 
 public class BufferManager {
-	private File file = null;
+	private RandomAccessFile file = null;
 	private int pageSize;
 	private int maxPageNumber;
 	private int lastFreePageNumber;
 	
-	void init(String filename, int pageSize) throws IOException {
-		file = new File("filename", "rb+");
+	public void init(String filename, int pageSize) throws IOException {
+		File file = new File("filename", "rb+");
 		if (!file.exists()) {
 			file.createNewFile();
 		}
 		
+		this.file = new RandomAccessFile(file, "rb+");
+		
 		this.pageSize = pageSize;
 		this.maxPageNumber = 0;
 		this.lastFreePageNumber = -1;
+	}
+	
+	public void close() throws IOException {
+		this.file.close();
 	}
 }
